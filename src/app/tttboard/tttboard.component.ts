86c0cb246b72039b9a9823a17d767aa518f400d1
stasz <stasznis02@gmail.com>
2023-10-12 16:38:09 +0200

    visual site

--- src/app/tttboard/tttboard.component.ts
+++ src/app/tttboard/tttboard.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tttboard.component.css']
 })
 export class TttboardComponent implements OnInit {
+  columns = [0,1,2];
+  rows = [0,1,2];
   square!: any[];
   xMove!: boolean;
   winner!: string;
@@ -35,49 +37,6 @@ export class TttboardComponent implements OnInit {
     }
   }
 
-  checkForWIn(): boolean {
-    // poziome
-    for (let i = 0; i < 9; i += 3){
-      let mark = this.square[i];
-      for (let j = 1; j < 3; j ++){
-        if(mark != this.square[i+j]){
-          break;
-        }
-        else{
-          if(j==2){
-            return true;
-          }
-        }
-      }
-    }
-    // pionowo
-    for (let i = 1; i < 3; i ++){    
-      let mark = this.square[i];
-      for (let j = 0; j < 9; j += 3){
-        if(mark != this.square[i+j]){
-          break;
-        }
-        else{
-          if(j==2){
-            return true;
-          }
-        }
-      }
-    }
-    // ma ukos
-    if(this.square[0] == 'X' && this.square[4] == 'X' && this.square[8] == 'X'){
-      return true;
-    }
-    if(this.square[0] == 'O' && this.square[4] == 'O' && this.square[8] == 'O'){
-      return true;
-    }
-    if(this.square[2] == 'X' && this.square[4] == 'X' && this.square[6] == 'X'){
-      return true;
-    }
-    if(this.square[2] == 'O' && this.square[4] == 'O' && this.square[6] == 'O'){
-      return true;
-    }
 
-    return false;
-  }
+
 }
